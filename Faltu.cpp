241eb0a461f867f3cd7af436c2e7287929ed90ef
haseharshit <haseharshit@gmail.com>
2021-11-09 11:27:01 +0530

    CPP added

---
+++ Faltu.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int k=1;
+    string S="";
+    char ch;
+    while(k){
+        cin>>ch;
+        string S+=ch;
+    }
+    cout<<"String is: "<<S;
+}
